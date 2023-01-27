dc4d23e6a9e05a2e744406e6ce8d45a61db29242
Daniel Nguyen <daniel.nguye@gmail.com>
2023-01-27 16:14:49 -0500

    generated devise views and controllers for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks'
+  }
   get 'about', to: 'about#index'
   root "home#index"
 end
