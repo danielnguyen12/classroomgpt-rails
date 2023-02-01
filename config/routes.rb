e263572436c20d19c73b4484c67f2387a63f5c16
Daniel Nguyen <daniel.nguye@gmail.com>
2023-02-01 11:10:05 -0500

    configured routes for image_request and story_request

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   get 'features', to: 'pages#features', as: :user_root
 
   get 'ai_images', to: 'ai_images#index'
+  post 'ai_images', to: 'ai_images#image_request'
   get 'ai_stories', to: 'ai_stories#index'
+  post 'ai_stories', to: 'ai_stories#story_request'
 
   root "pages#home"
 end
