b421eccf476fcd35469a958f1325a715fc40aca9
Daniel Nguyen <daniel.nguye@gmail.com>
2023-02-01 11:19:55 -0500

    added image_request and params

--- app/controllers/ai_images_controller.rb
+++ app/controllers/ai_images_controller.rb
@@ -3,4 +3,22 @@ class AiImagesController < ApplicationController
 
   def index
   end
+
+  def image_request
+    client = OpenAI::Client.new
+    prompt = image_request_params[:prompt]
+    response = client.images.generate(
+      parameters: {
+        prompt: prompt,
+        size: "256x256"
+        }
+      )
+    puts response.dig("data", 0, "url")
+  end
+
+  private
+
+  def image_request_params
+    params.require(:image_request).permit(:prompt)
+  end
 end
