bd84aeb4c6e3316c74214b94c29a265ff4860da6
Daniel Nguyen <daniel.nguye@gmail.com>
2023-02-02 16:27:59 -0500

    image generation is working, commented out to prevent charges

--- app/controllers/ai_images_controller.rb
+++ app/controllers/ai_images_controller.rb
@@ -9,31 +9,45 @@ class AiImagesController < ApplicationController
     @ai_image = AiImage.new
   end
 
-  def create
-    # client = OpenAI::Client.new
-    prompt = ai_image_params[:prompt]
-    puts prompt
-    # response = client.images.generate(
-    #   parameters: {
-    #     prompt: prompt,
-    #     size: "256x256"
-    #     }
-    #   )
-    # puts response.dig("data", 0, "url")
+  # Create action is working as expected. Commented out to save money
+  # def create
+  #   client = OpenAI::Client.new
+  #   prompt = image_request_params[:prompt]
+  #   response = client.images.generate(
+  #     parameters: {
+  #       prompt: prompt,
+  #       size: "256x256"
+  #       }
+  #     )
+      # if the image generation is successful, save the prompt and image URL to ai_image object
+  #   if response.success?
+  #     puts response.dig("data", 0, "url")
+  #     @img_url = response["data"].first["url"]
+  #     @ai_image = AiImage.new(user: current_user, prompt: @prompt, img_url: @img_url)
 
-    # if the response is successful, create new AiImage object in database
+  #     if @ai_image.save
+  #       redirect_to ai_images_path, notice: 'AI Image was successfully created.'
+  #     else
+  #       render :new, notice: :unprocessable_entity
+  #     end
+  #   else
+  #     error_message = response.body["message"]
+  #     redirect_to new_ai_image_path, alert: "Failed to generate image: #{error_message}"
+  #   end
+
+  def create #create action for testing
+    prompt = image_request_params[:prompt]
     @ai_image = AiImage.create!(user: current_user, prompt: prompt, img_url: "https://cdn.pixabay.com/photo/2014/06/03/19/38/board-361516__340.jpg")
-
-    if @ai_image.save
-      redirect_to ai_images_path, notice: 'AI Image was successfully created.'
-    else
-      render :new, notice: :unprocessable_entity
-    end
+      if @ai_image.save
+        redirect_to ai_images_path, notice: 'AI Image was successfully created.'
+      else
+        render :new, notice: :unprocessable_entity
+      end
   end
 
   private
 
-  def ai_image_params
+  def image_request_params
     params.require(:ai_image).permit(:prompt)
   end
 end
